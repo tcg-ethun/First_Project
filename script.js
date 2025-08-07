4a615d2c17abde3321f1b2d7db95229eac49ec9d
tcg-ethun <ethuntcg@gmail.com>
2025-08-07 18:25:04 +0600

    Updated

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function alr(){
+    alert(" This is our First Html Project . We Build it Using only Html . Hahahhh :]  Happy Coding 😉");
+}
+
+function back(){
+    window.location.href='index.html'
+}
